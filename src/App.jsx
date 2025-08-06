80fccdd4f8378e5ad07fd174556cd215956e8f89
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-08-06 18:22:17 -0300

    Add Familia and FamiliaMembro components
    Introduced new components Familia and FamiliaMembro to demonstrate component composition and prop passing. Updated App.jsx to include a new Card showcasing the Familia component with sample members.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import "./App.css"
+import './App.css';
 import React from 'react';
 
 import Card from './components/layout/Card';
+import Familia from './components/basicos/Familia';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
@@ -12,25 +13,31 @@ export default () => (
     <h1>Fundamentos React (Arrow Function)</h1>
 
     <div className="Cards">
-        <Card titulo="#04 Desafio Aleatório" color="#FA6900">
+      <Card titulo="#05 Componente com Filhos" color="#D0C91F">
+        <Familia sobrenome="Fernandes">
+          
+        </Familia>
+      </Card>
+
+      <Card titulo="#04 Desafio Aleatório" color="#FA6900">
         <Aleatorio min={1} max={60} />
-        </Card>
+      </Card>
 
-        <Card titulo="#03 Fragmento" color="#E94E69">
+      <Card titulo="#03 Fragmento" color="#E94E69">
         <Fragmento />
-        </Card>
+      </Card>
 
-        <Card titulo="#02 Com Parâmetro" color="#28ABE3">
+      <Card titulo="#02 Com Parâmetro" color="#28ABE3">
         <ComParametro
           titulo="Situação do Aludo"
           aluno="Dennys Fernandes"
-          nota={9.3}/>
-        </Card>
+          nota={9.3}
+        />
+      </Card>
 
-        <Card titulo="#01 Primeiro Componente" color="#588C73">
+      <Card titulo="#01 Primeiro Componente" color="#588C73">
         <Primeiro></Primeiro>
-        </Card>
+      </Card>
     </div>
-
   </div>
 );
