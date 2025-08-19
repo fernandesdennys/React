bffcacaac2f522cad59ca7295be12ec474ea49d9
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-08-19 11:41:03 -0300

    Add Param and NotFound routes and components
    Introduced new Param and NotFound components for dynamic and fallback routing. Updated Content.jsx to include these routes and enhanced Menu.jsx with links to demonstrate parameterized and non-existent routes. Minor formatting improvements were also made to About.jsx.

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -1,19 +1,22 @@
-import "./Content.css";
-import React from "react";
-import { Routes, Route } from "react-router-dom"; // Importe esses arquivos
- 
-import Home from "../../views/expamples/Home";
-import About from "../../views/expamples/About";
+import './Content.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'; // Importe esses arquivos
 
- 
-const Content = props => (
+import Home from '../../views/expamples/Home';
+import About from '../../views/expamples/About';
+import Param from '../../views/expamples/Param';
+import NotFound from '../../views/expamples/NoFound';
+
+const Content = (props) => (
   <main className="Content">
     {/* Toda essa parte está diferente da do professor, analisem as diferenças */}
     <Routes>
-      <Route path="/about" element={<About />} />
       <Route path="/" exact element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/param/:id" element={<Param />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   </main>
 );
- 
+
 export default Content;
--- navegacao/src/components/layout/Menu.jsx
+++ navegacao/src/components/layout/Menu.jsx
@@ -3,18 +3,31 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-const Menu = porps => (
-<aside className="Menu">
-  <nav>
-    <ul>
-      <li> 
-        <Link to="/">Início</Link>
-      </li>
-      <li>
-        <Link to="/about">Sobre</Link>
-      </li>
-    </ul>
-  </nav>
-</aside>
-)
+const Menu = (porps) => (
+  <aside className="Menu">
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Início</Link>
+        </li>
+
+        <li>
+          <Link to="/param/Dennys">Param #01</Link>
+        </li>
+
+        <li>
+          <Link to="/param/Fernandes">Param #02</Link>
+        </li>
+
+        <li>
+          <Link to="/about">Sobre</Link>
+        </li>
+
+        <li>
+          <Link to="/naoexiste">Não Existe</Link>
+        </li>
+      </ul>
+    </nav>
+  </aside>
+);
 export default Menu;
