7f0f5998659f6d91fc1c490dc954a9eed8f57b47
Dennys Fernandes <dennys.projetos@outlook.com>
2025-08-28 15:26:11 -0300

    Add useFetch hook and integrate with UseCustom example
    Introduced a new useFetch custom hook for data fetching with loading state. Updated the UseCustom example to demonstrate fetching and displaying a list of states using the new hook.

--- hooks/src/views/expamples/UseCustom.jsx
+++ hooks/src/views/expamples/UseCustom.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import PageTitle from '../../components/layout/PageTitle';
 import SectionTitle from './../../components/layout/SectionTitle';
 import { useCounter } from '../../hooks/useCounter';
+import { useFetch } from '../../hooks/useFetch';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const UseRef = (props) => {
+
   const [count, inc, dec] = useCounter();
+  const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+  const response = useFetch(url)
+
+  function showStates(states) {
+    return states.map(state => <li>{state.nome} - {state.sigla}</li>)
+  }
+
+
   return (
     <div className="UseCustom">
       <PageTitle
@@ -22,6 +33,12 @@ const UseRef = (props) => {
             Dec
           </button>
         </div>
+      </div>
+      <SectionTitle title="Execício #02" />
+      <div className="center">
+        <ul>
+          {response.data ? showStates(response.data) : false}
+        </ul>
       </div>
     </div>
   );
